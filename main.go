6abdad045b187d471ce1976f03ebf89d8d7ba37d
masibw <masi19bw@gmail.com>
2022-02-02 10:20:08 +0900

    feat: added options to get test output file name.

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello goppa!")
+	app := &cli.App{
+		Name:  "goppa",
+		Usage: "compare test time.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name: "previous",
+				Aliases: []string{
+					"p",
+				},
+				Usage:    "specify previous test output file.(json)",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name: "current",
+				Aliases: []string{
+					"c",
+				},
+				Usage:    "specify current test output file.(json)",
+				Required: true,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			fmt.Println(c.String("previous"), c.String("p"), c.String("current"))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
