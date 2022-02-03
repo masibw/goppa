89a2b5fa059ad738c3ba2143508a39116f5b2c1c
masibw <masi19bw@gmail.com>
2022-02-03 13:09:57 +0900

    refactor: move compare statement to TestEvent's method (#4)
    * refactor: move compare statement to TestEvent's method
    
    * refactor: to simple

--- domain/valueobject/testEvent.go
+++ domain/valueobject/testEvent.go
@@ -4,3 +4,7 @@ type TestEvent struct {
 	Name    string
 	Elapsed float64 // Elapsed is the time taken for the test, expressed in seconds.
 }
+
+func (t *TestEvent) IsSlowerThan(prev float64, border float64) bool {
+	return t.Elapsed > prev*border
+}
